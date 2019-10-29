a90651df9b0797eb15ad808c12572a0f972be70f
Adel Abdellatif <Adel.A.Abdelkader@gmail.com>
2019-10-30 01:21:36 +0200

    connect to the database

---
+++ Logs-Analysis-Report.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# Logs Analysis Project
+
+# Import postgresql library
+import psycopg2
+
+# connecting and executing the queries in the database
+DBNAME = "news"
+
+
+def queryDB(query):
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute(query)
+    results = c.fetchall()
+    db.close()
+    return results
